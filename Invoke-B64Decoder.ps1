4f699ad0ddda42d2d7e9b04eacade186e301f5dc
lyethar <107083757+lyethar@users.noreply.github.com>
2023-10-25 10:19:03 -0400

    Update Invoke-B64Decoder.ps1

--- Invoke-B64Decoder.ps1
+++ Invoke-B64Decoder.ps1
@@ -2,7 +2,10 @@ function DecodeBase64($base64String) {
     return [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($base64String))
 }
 
-# Example usage:
-$encodedString = "WwBOAGUAdAAuAFMAZQByAHYA..."
-$decodedString = DecodeBase64 $encodedString
+if ($args.Count -ne 1) {
+    Write-Error "Please provide a single Base64 encoded string as an argument."
+    exit 1
+}
+
+$decodedString = DecodeBase64 $args[0]
 Write-Output $decodedString

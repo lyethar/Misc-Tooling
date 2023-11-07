e4f487779ca27f018a25688c928a106af94999ec
lyethar <107083757+lyethar@users.noreply.github.com>
2023-11-06 22:33:10 -0500

    Update mitm6_looper.py

--- mitm6_looper.py
+++ mitm6_looper.py
@@ -1,21 +1,21 @@
 import subprocess
 import time
 import signal
-import os
 
 # Define the command and arguments
-command = ['sudo', 'mitm6', '-d', 'domain.local', '-ignore-nofqdn', '-i', 'eth0']
+command = ['sudo', 'mitm6', '-d', 'hmi.local', '-ignore-nofqdn', '-i', 'eth0']
 
-# Define the wait time of 5 minutes in seconds
-wait_time = 5 * 60
+# Define the wait time of 10 minutes in seconds
+wait_time_between_commands = 10 * 60
+wait_time_during_command = 5 * 60
 
 while True:
     # Start the process
     print("Starting the mitm6 command.")
     process = subprocess.Popen(command)
 
-    # Wait for 5 minutes
-    time.sleep(wait_time)
+    # Wait for 5 minutes while the command is running
+    time.sleep(wait_time_during_command)
 
     # Terminate the process
     print("Stopping the mitm6 process.")
@@ -29,6 +29,6 @@ while True:
         process.kill()  # Forcefully kill the process if it didn't terminate
         process.wait()  # Now we wait again to make sure it's killed
 
-    # Wait another 5 minutes
-    print("Waiting for 5 minutes before restarting the command.")
-    time.sleep(wait_time)
+    # Wait for 10 minutes before restarting the command
+    print("Waiting for 10 minutes before restarting the command.")
+    time.sleep(wait_time_between_commands)
